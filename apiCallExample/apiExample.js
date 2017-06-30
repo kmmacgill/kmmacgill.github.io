84838f64aff675bfd24d1c912df9d47a1248a94f
Korey <kmmacgill@gmail.com>
2017-06-30 14:23:43 -0600

    testing api call code on dnd api to see if it supports cors

--- apiCallExample/apiExample.js
+++ apiCallExample/apiExample.js
@@ -2,8 +2,17 @@
  * Created by kmmac on 6/30/2017.
  */
 function getJSON(part) {
-    var url = "http://dnd5eapi.co/api/";
-    return fetch(url+part)
+    var url = "https://dnd5eapi.co/api/";
+    //FETCH INIT
+    var myHeader = new Headers();
+
+    var myInit = { method: 'GET',
+                   headers: myHeader,
+                   mode: 'cors',
+                   cache: 'default' };
+    var myRequest = new Request(url + part, myInit);
+
+    return fetch(myRequest)
         .then(function (response) {
             response.forEach(function(element) {
                 createElement(element);
@@ -12,6 +21,35 @@ function getJSON(part) {
         .catch(function (error) {
             console.log(error);
         });
+}
+
+function createCORSRequest(method, part) {
+    var url = "https://dnd5eapi.co/api/" + part;
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+
+        // Check if the XMLHttpRequest object has a "withCredentials" property.
+        // "withCredentials" only exists on XMLHTTPRequest2 objects.
+        xhr.open(method, url, true);
+
+    } else if (typeof XDomainRequest != "undefined") {
+
+        // Otherwise, check if XDomainRequest.
+        // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+
+    } else {
+
+        // Otherwise, CORS is not supported by the browser.
+        xhr = null;
+
+    }
+    if (!xhr) {
+        var chaoticList = document.getElementsByClassName('chaos_list');
+        chaoticList.innerHTML = "";
+        chaoticList.innerHTML = "CORS NOT SUPPORTED"
+    }
 }
 
 function createElement (element) {
